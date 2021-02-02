2e77a2e11aded22d1ada6cb93c37203a5d71f738
lambertwijshakecreations <73013216+lambertwijshakecreations@users.noreply.github.com>
2021-02-02 20:58:50 +0100

    Add files via upload

---
+++ array.js
@@ -0,0 +1,19 @@
+/*let colors = ["yellow", "blue", "red", "orange"];
+let i = 0;
+while (i < 6){
+    console.log(colors);
+    i++
+}
+
+for(i=0; i<6; i++){
+    console.log(colors);
+    Array.length
+}
+*/
+
+
+let colors = ["yellow", "blue", "red", "orange"];
+colors.forEach(element => console.log(element));
+
+
+
